447e25a8d2ddf26f1d188872c4a7b14e4c9342af
Helge <helgeahrens@Helges-MacBook-Pro.local>
2024-05-02 11:50:28 +0200

    update query

--- blocks/query/query.js
+++ blocks/query/query.js
@@ -1,5 +1,5 @@
 /** @param {HTMLElement} block */
 export default async function decorate(block) {
-  const api = await fetch('https://main--aem-universaleditor--tylopilus.hlx.live/query-index.json').then((res) => res.json());
+  const api = await fetch('/query-index.json').then((res) => res.json());
   block.innerHTML = `<pre>${JSON.stringify(api, null, 2)}</pre>`;
 }
