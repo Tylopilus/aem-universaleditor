d9a7e1c731291546112ab97f60c01a1e3a04e354
Helge <helgeahrens@Helges-MacBook-Pro.local>
2024-06-20 16:15:52 +0200

    test

--- scripts/configs.js
+++ scripts/configs.js
@@ -25,7 +25,7 @@ export const calcEnvironment = () => {
 
 function buildConfigURL(environment) {
   const env = environment || calcEnvironment();
-  const configURL = new URL(`${window.location.origin}/configs.json`);
+  const configURL = new URL(`${window.location.origin}/query-index.json`);
   // configURL.searchParams.set('sheet', env);
   return configURL;
 }
