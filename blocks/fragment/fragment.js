eb499fabb0c2e5923d72951013733e0b8599917a
Helge <helgeahrens@Helges-MacBook-Pro.local>
2024-06-21 11:05:07 +0200

    make it work for pdp

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -4,13 +4,9 @@
  * https://www.aem.live/developer/block-collection/fragment
  */
 
-import {
-  decorateMain,
-} from '../../scripts/scripts.js';
+import { decorateMain } from '../../scripts/scripts.js';
 
-import {
-  loadBlocks,
-} from '../../scripts/aem.js';
+import { loadBlocks } from '../../scripts/aem.js';
 
 /**
  * Loads a fragment.
@@ -19,7 +15,12 @@ import {
  */
 export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
-    const resp = await fetch(`${path}.plain.html`);
+    const fetchPath = window.location.href.includes(
+      'adobeaemcloud.com/products/'
+    )
+      ? `/content/helge-univeral-editor${path}.plain.html`
+      : `${path}.plain.html`;
+    const resp = await fetch(fetchPath);
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
@@ -27,7 +28,10 @@ export async function loadFragment(path) {
       // reset base path for media to fragment base
       const resetAttributeBase = (tag, attr) => {
         main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+          elem[attr] = new URL(
+            elem.getAttribute(attr),
+            new URL(path, window.location)
+          ).href;
         });
       };
       resetAttributeBase('img', 'src');
