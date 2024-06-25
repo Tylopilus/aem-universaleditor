e30a2f003ba34149a0852ef9f14c195558d34c18
Helge <helgeahrens@Helges-MacBook-Pro.local>
2024-06-25 11:42:18 +0200

    this

--- blocks/product-teaser/product-teaser.js
+++ blocks/product-teaser/product-teaser.js
@@ -1,5 +1,8 @@
 import { readBlockConfig } from '../../scripts/aem.js';
-import { performCatalogServiceQuery, renderPrice } from '../../scripts/commerce.js';
+import {
+  performCatalogServiceQuery,
+  renderPrice,
+} from '../../scripts/commerce.js';
 
 const productTeaserQuery = `query productTeaser($sku: String!) {
   products(skus: [$sku]) {
@@ -46,7 +49,8 @@ fragment priceFields on ProductViewPrice {
 
 function renderPlaceholder(config, block) {
   block.textContent = '';
-  block.appendChild(document.createRange().createContextualFragment(`
+  block.appendChild(
+    document.createRange().createContextualFragment(`
     <div class="image">
       <div class="placeholder"></div>
     </div>
@@ -54,11 +58,20 @@ function renderPlaceholder(config, block) {
       <h1></h1>
       <div class="price"></div>
       <div class="actions">
-        ${config['details-button'] ? '<a href="#" class="button primary disabled">Details</a>' : ''}
-        ${config['cart-button'] ? '<button class="secondary" disabled>Add to Cart</button>' : ''}
+        ${
+          config['details-button']
+            ? '<a href="#" class="button primary disabled">Details</a>'
+            : ''
+        }
+        ${
+          config['cart-button']
+            ? '<button class="secondary" disabled>Add to Cart</button>'
+            : ''
+        }
       </div>
     </div>
-  `));
+  `)
+  );
 }
 
 function renderImage(image, size = 250) {
@@ -78,7 +91,12 @@ function renderImage(image, size = 250) {
     return newUrl.toString();
   };
 
-  const createUrlForDpi = (url, w, useWebply = true) => `${createUrlForWidth(url, w, useWebply)} 1x, ${createUrlForWidth(url, w * 2, useWebply)} 2x, ${createUrlForWidth(url, w * 3, useWebply)} 3x`;
+  const createUrlForDpi = (url, w, useWebply = true) =>
+    `${createUrlForWidth(url, w, useWebply)} 1x, ${createUrlForWidth(
+      url,
+      w * 2,
+      useWebply
+    )} 2x, ${createUrlForWidth(url, w * 3, useWebply)} 3x`;
 
   const webpUrl = createUrlForDpi(imageUrl, size, true);
   const jpgUrl = createUrlForDpi(imageUrl, size, false);
@@ -86,17 +104,22 @@ function renderImage(image, size = 250) {
   return document.createRange().createContextualFragment(`<picture>
       <source srcset="${webpUrl}" />
       <source srcset="${jpgUrl}" />
-      <img height="${size}" width="${size}" src="${createUrlForWidth(imageUrl, size, false)}" loading="eager" alt="${label}" />
+      <img height="${size}" width="${size}" src="${createUrlForWidth(
+    imageUrl,
+    size,
+    false
+  )}" loading="eager" alt="${label}" />
     </picture>
   `);
 }
 
 function renderProduct(product, config, block) {
-  const {
-    name, urlKey, sku, price, priceRange, addToCartAllowed, __typename,
-  } = product;
+  const { name, urlKey, sku, price, priceRange, addToCartAllowed, __typename } =
+    product;
 
-  const currency = price?.final?.amount?.currency || priceRange?.minimum?.final?.amount?.currency;
+  const currency =
+    price?.final?.amount?.currency ||
+    priceRange?.minimum?.final?.amount?.currency;
   const priceFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency,
@@ -110,25 +133,43 @@ function renderProduct(product, config, block) {
       <h1>${name}</h1>
       <div class="price">${renderPrice(product, priceFormatter.format)}</div>
       <div class="actions">
-        ${config['details-button'] ? `<a href="/products/${urlKey}/${sku}" class="button primary">Details</a>` : ''}
-        ${config['cart-button'] && addToCartAllowed && __typename === 'SimpleProductView' ? '<button class="add-to-cart secondary">Add to Cart</button>' : ''}
+        ${
+          config['details-button']
+            ? `<a href="/products/${urlKey}/${sku}" class="button primary">Details</a>`
+            : ''
+        }
+        ${
+          config['cart-button'] &&
+          addToCartAllowed &&
+          __typename === 'SimpleProductView'
+            ? '<button class="add-to-cart secondary">Add to Cart</button>'
+            : ''
+        }
       </div>
     </div>
   `);
 
-  fragment.querySelector('.image').appendChild(renderImage(product.images[0], 250));
+  fragment
+    .querySelector('.image')
+    .appendChild(renderImage(product.images[0], 250));
 
   const addToCartButton = fragment.querySelector('.add-to-cart');
   if (addToCartButton) {
     addToCartButton.addEventListener('click', async () => {
-      const values = [{
-        optionsUIDs: [],
-        quantity: 1,
-        sku: product.sku,
-      }];
-      const { addProductsToCart } = await import('@dropins/storefront-cart/api.js');
+      const values = [
+        {
+          optionsUIDs: [],
+          quantity: 1,
+          sku: product.sku,
+        },
+      ];
+      const { addProductsToCart } = await import(
+        '@dropins/storefront-cart/api.js'
+      );
       // TODO: productId not exposed by catalog service as number
-      window.adobeDataLayer.push({ productContext: { productId: 0, ...product } });
+      window.adobeDataLayer.push({
+        productContext: { productId: 0, ...product },
+      });
       console.debug('onAddToCart', values);
       addProductsToCart(values);
     });
@@ -139,8 +180,12 @@ function renderProduct(product, config, block) {
 
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-  config['details-button'] = !!(config['details-button'] || config['details-button'] === 'true');
-  config['cart-button'] = !!(config['cart-button'] || config['cart-button'] === 'true');
+  config['details-button'] = !!(
+    config['details-button'] || config['details-button'] === 'true'
+  );
+  config['cart-button'] = !!(
+    config['cart-button'] || config['cart-button'] === 'true'
+  );
 
   renderPlaceholder(config, block);
 
@@ -151,7 +196,12 @@ export default async function decorate(block) {
     return;
   }
   const [product] = products;
-  product.images = product.images.map((image) => ({ ...image, url: image.url.replace(/^https?:/, '') }));
+  product.images = product.images.map((image) => ({
+    ...image,
+    url: image.url.replace(/^https?:/, ''),
+  }));
 
-  renderProduct(product, config, block);
+  const div = document.createElement('div');
+  block.appendChild(div);
+  renderProduct(product, config, div);
 }
